6dd7db0be2181f42febfcb0ba46eab4e4a4f0197
myselfHimanshu <047himanshu@gmail.com>
2018-06-19 16:17:15 +0530

    dicee image changes

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Himanshu Panwar on 17/06/18.
+//  Copyright © 2018 Himanshu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //print("Roll Button Pressed...")
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        //get a random number
+        randomDiceIndex1 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        randomDiceIndex2 = Int(arc4random_uniform(UInt32(diceArray.count)))
+        
+        //call the dice image
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+}
+
