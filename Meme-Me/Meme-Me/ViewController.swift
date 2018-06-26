56b8dcf846ee6303fbb859069af67e5d57c5ea5a
myselfHimanshu <047himanshu@gmail.com>
2018-06-26 15:44:11 +0530

    sliders

--- Meme-Me/Meme-Me/ViewController.swift
+++ Meme-Me/Meme-Me/ViewController.swift
@@ -10,39 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0
-    @IBOutlet weak var label : UILabel!
-    @IBOutlet weak var colorView : UIView!
-    @IBOutlet weak var redControl : UISwitch!
-    @IBOutlet weak var greenControl : UISwitch!
-    @IBOutlet weak var blueControl : UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    @IBAction func incrementCount(){
-        self.count += 1
-        self.label.text = "\(self.count)"
-    }
-    
-    @IBAction func decrementCount(){
-        self.count -= 1
-        self.label.text = "\(self.count)"
-    }
-    
-    @IBAction func changeColor(){
-        if self.redControl==nil{
-            return
-        }
-        
-        let r : CGFloat = self.redControl.isOn ? 1 : 0
-        let g : CGFloat = self.greenControl.isOn ? 1 : 0
-        let b : CGFloat = self.blueControl.isOn ? 1 : 0
-        
-        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
-    }
-    
 
 
 }
